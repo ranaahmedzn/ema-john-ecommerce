993d56203b220fa70b3c201488b41130a04db758
Rana-Sheikh <ranasheikhsbd@gmail.com>
2023-04-18 09:15:30 +0600

    design login and sign up form

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,10 +1,33 @@
 import React from 'react';
 import './Login.css'
+import { Link } from 'react-router-dom';
+import google from '../../assets/google.png'
 
 const Login = () => {
     return (
-        <div>
-            <h3>Login page coming soon...</h3>
+        <div className='form-container'>
+            <h3 className='form-title'>Login</h3>
+            <form>
+                <div className="form-control">
+                    <label htmlFor="email">Email:</label>
+                    <input type="email" name="email" id="email" />
+                </div>
+                <div className="form-control">
+                    <label htmlFor="password">Password:</label>
+                    <input type="password" name="password" id="password" />
+                </div>
+                <input className='btn-submit' type="submit" value="Login" />
+                <p className='form-question'>New to Ema-John? <span><Link to="/signup">Create New Account</Link></span></p>
+                <p className='or-line'>
+                    <span></span>
+                    Or
+                    <span></span>
+                </p>
+                <div className='continue-with-google'>
+                    <img src={google} alt="" />
+                    <span>Continue with Google</span>
+                </div>
+            </form>
         </div>
     );
 };
--- src/components/SignUp/SignUp.jsx
+++ src/components/SignUp/SignUp.jsx
@@ -1,10 +1,37 @@
 import React from 'react';
 import './SignUp.css'
+import { Link } from 'react-router-dom';
+import google from '../../assets/google.png'
 
 const SignUp = () => {
     return (
-        <div>
-            <h2>This is Sign up page</h2>
+        <div className='form-container'>
+            <h3 className='form-title'>Sign Up</h3>
+            <form>
+                <div className="form-control">
+                    <label htmlFor="email">Email:</label>
+                    <input type="email" name="email" id="email" />
+                </div>
+                <div className="form-control">
+                    <label htmlFor="password">Password:</label>
+                    <input type="password" name="password" id="password" />
+                </div>
+                <div className="form-control">
+                    <label htmlFor="confirm-password">Confirm Password:</label>
+                    <input type="password" name="confirm" id="confirm-password" />
+                </div>
+                <input className='btn-submit' type="submit" value="Sign Up" />
+                <p className='form-question'>Already have an account? <span><Link to="/login">Login</Link></span></p>
+                <p className='or-line'>
+                    <span></span>
+                    Or
+                    <span></span>
+                </p>
+                <div className='continue-with-google'>
+                    <img src={google} alt="" />
+                    <span>Continue with Google</span>
+                </div>
+            </form>
         </div>
     );
 };
